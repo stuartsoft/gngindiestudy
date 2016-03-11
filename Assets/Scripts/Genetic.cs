4694185a74416925bcc46fda54c8b2afa8fb8658
Stuart Bowman <stuartsoft2012@gmail.com>
2016-03-11 01:30:20 -0500

    Began reworking stuff to migrate all code to unity

--- Assets/Scripts/Genetic.cs
+++ Assets/Scripts/Genetic.cs
@@ -2,20 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Node
-{
-    public int ID;
-    public Vector2 pos;
-    public List<Node> connectedNodes;
-
-    public Node(Vector2 position, int id)
-    {
-        connectedNodes = new List<Node>();
-        pos = position;
-        ID = id;
-    }
-}
-
 public class Genetic : MonoBehaviour {
 
     public float UnitHeight = 1;
@@ -24,68 +10,20 @@ public class Genetic : MonoBehaviour {
     public List<Unit> units;
     public List<Edge> edges;
 
-    public Dictionary<int, Node> nodes;
-
     void Start()
     {
+        Graph g = new Graph(50);
+        g.printAdjMatrix();
+
+        /*
         //set up data structures
         units = new List<Unit>();
         edges = new List<Edge>();
 
-        nodes = new Dictionary<int, Node>();
-
-        string line;
-        System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\bowmanrs1\\IdeaProjects\\Waypoint\\output.txt");
-        while((line = file.ReadLine()) != null)
-        {
-            string[] toks = line.Split(' ');
-            if (toks[0] == "Node:")
-            {
-                int ID;
-                int.TryParse(toks[1],out ID);
-                float x;
-                float.TryParse(toks[2], out x);
-                float y;
-                float.TryParse(toks[3], out y);
-                Node n = new Node(new Vector2(x, y), ID);
-                nodes.Add(ID, n);
-                //Debug.Log("New node: " + ID);
-            }
-        }
-        file.Close();
-
-        System.IO.StreamReader file2 = new System.IO.StreamReader("C:\\Users\\bowmanrs1\\IdeaProjects\\Waypoint\\output.txt");
-        while ((line = file2.ReadLine()) != null)
-        {
-            string[] toks = line.Split(' ');
-            if (toks[0] == "Node:")
-            {
-                line = file2.ReadLine();
-                string[] toks2 = line.Split(' ');
-                int ID = int.Parse(toks[1]);
-                if (toks2[0] == "Conn:")
-                {
-                    Node tempn;
-                    nodes.TryGetValue(ID, out tempn);
-                    for (int i = 1; i < toks2.Length; i++)
-                    {
-                        if (toks2[i] == "") break;
-                        int connID;
-                        int.TryParse(toks2[i], out connID);
-                        Node connectedNode;
-                        nodes.TryGetValue(connID, out connectedNode);
-                        tempn.connectedNodes.Add(connectedNode);
-                        //Debug.Log("ConnID: " + connID);
-                    }
-                }
-            }
-        }
-        file.Close();
-
         //first create all node game objects
         foreach(KeyValuePair<int, Node> entry in nodes)
         {
-            Vector2 setPos = new Vector2((float)GetComponent<BuildMaze>().getWidth() - (entry.Value.pos.x-0.5f), (float)GetComponent<BuildMaze>().getHeight() - (entry.Value.pos.y-0.5f));
+            Vector2 setPos = new Vector2((entry.Value.pos.x-0.5f), (entry.Value.pos.y-0.5f));
             Unit u = addNode(entry.Key, entry.Value.pos);
         }
 
@@ -127,11 +65,12 @@ public class Genetic : MonoBehaviour {
             edges[i].mDrawnLine.GetComponent<LineRenderer>().SetPosition(0, edges[i].mPointA.mCenter - new Vector3(0.0f, 0.1f, 0.0f));
             edges[i].mDrawnLine.GetComponent<LineRenderer>().SetPosition(1, edges[i].mPointB.mCenter - new Vector3(0.0f, 0.1f, 0.0f));
         }
+        */
     }
 
     public Unit addNode(int id, Vector2 pos)
     {
-        Unit point = new Unit(new Vector3(pos.x*2, UnitHeight, pos.y*2 ),id, UnitMarker);
+        Unit point = new Unit(new Vector3(pos.x*2, UnitHeight, (float)GetComponent<BuildMaze>().getHeight() - pos.y*2 ),id, UnitMarker);
         units.Add(point);
         return point;
     }
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Graph
+{
+    public Dictionary<int, Node> nodes;
+
+    public Graph(int initialNodes)
+    {
+        nodes = new Dictionary<int, Node>();
+        for (int i = 0; i < initialNodes; i++)
+        {
+            Node n = addNewNode(new Vector2(0,0));
+            n.connectTo(nodes[0]);
+        }
+
+        removeNode(5);
+
+    }
+
+    public int getFirstUnusedID()
+    {
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if (!nodes.ContainsKey(i))
+                return i;
+        }
+        return nodes.Count;
+    }
+
+    public int maxID
+    {
+        get
+        {
+            int temp = 0;
+            foreach (KeyValuePair<int, Node> entry in nodes)
+            {
+                if (entry.Value.ID > temp) temp = entry.Value.ID;
+            }
+            return temp;
+        }
+    }
+
+    public class Node
+    {
+        public int ID;
+        public Vector2 pos;
+        public List<Node> connectedNodes;
+
+        public Node(Vector2 position, int id)
+        {
+            connectedNodes = new List<Node>();
+            pos = position;
+            ID = id;
+        }
+
+        public void connectTo(Node node)
+        {
+            if (!connectedNodes.Contains(node))
+            {
+                connectedNodes.Add(node);
+                if (!node.connectedNodes.Contains(this))
+                {
+                    node.connectedNodes.Add(this);
+                }
+            }
+        }
+
+        public void disconnectFrom(Node n)
+        {
+            for (int i = 0; i < connectedNodes.Count; i++)
+            {
+                if (connectedNodes[i] == n)
+                    n.connectedNodes.Remove(this);
+            }
+
+            connectedNodes.Remove(n);
+        }
+    }
+
+    public Node addNewNode(Vector2 pos)
+    {
+        int newID = getFirstUnusedID();
+        Node tempNode = new Node(pos, newID);
+        nodes.Add(newID, tempNode);
+        return tempNode;
+    }
+
+    public void removeNode(int ID)
+    {
+        Node nodeToRemove = nodes[ID];
+        foreach (Node n in nodeToRemove.connectedNodes)
+        {
+            //remove symmetrical connections
+            n.connectedNodes.Remove(nodeToRemove);
+        }
+        nodes.Remove(ID);//delete the actual node
+    }
+
+    public void printAdjMatrix()
+    {
+        foreach (KeyValuePair<int, Node> entry in nodes)
+        {
+            string connNodes = "Node: " + entry.Value.ID + "\nConn: ";
+            foreach (Node n2 in entry.Value.connectedNodes)
+            {
+                connNodes += n2.ID + ", ";
+            }
+            Debug.Log(connNodes);
+        }
+    }
+
+}
